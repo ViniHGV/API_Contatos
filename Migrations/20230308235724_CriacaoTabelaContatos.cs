89f075ecad6420651abfc364e784829a092e027f
Vini <ixxvinicius@gmail.com>
2023-03-08 21:09:24 -0300

    first commit

---
+++ Migrations/20230308235724_CriacaoTabelaContatos.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CRUD_API.Migrations
+{
+    public partial class CriacaoTabelaContatos : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Contatos",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Telefone = table.Column<int>(type: "int", nullable: false),
+                    Ativo = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contatos", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contatos");
+        }
+    }
+}
---
+++ Models/Contato.cs
@@ -0,0 +1,10 @@
+﻿namespace CRUD_API.Models
+{
+    public class Contato
+    {
+        public int id { get; set; }
+        public string Nome { get; set; }
+        public int Telefone { get; set; }
+        public bool Ativo { get; set; }
+    }
+}
