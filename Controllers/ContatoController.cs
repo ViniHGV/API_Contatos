858dac15d8e644dcf48142968e580f99821f931e
Vini <ixxvinicius@gmail.com>
2023-03-08 22:02:30 -0300

    Finalizando o endpoint Obter por nome

--- Controllers/ContatoController.cs
+++ Controllers/ContatoController.cs
@@ -37,6 +37,13 @@ namespace CRUD_API.Controllers
             return Ok(contato);
         }
 
+        [HttpGet("ObterPorNome")]
+        public IActionResult ObterPorNome(string nome)
+        {
+            var contatos = _context.Contatos.Where(x => x.Nome.Contains(nome));
+            return Ok(contatos);
+        }
+
         [HttpPut("{id}")]
         public IActionResult Atualizar(int id, Contato contato)
         {
